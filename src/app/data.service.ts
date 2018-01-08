ccb4e7a26ee7edbdbf3f78e65f04180ed9bf0ab2
sngvhamed <ahmednasser1993@live.com>
2018-01-08 13:29:53 +0200

    make crediontal for cookies

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers } from '@angular/http';
+import { Http, Response, Headers ,RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { Configuration } from './configuration';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class DataService<Type> {
@@ -14,26 +15,29 @@ export class DataService<Type> {
     private connect_sid: string = 'connect.sid';
     private access_token: string = 'access_token';
     private userId: string = 'userId';
+    private _cookieService:CookieService;
 
     public createKey(key: string){
-        if(localStorage.getItem(key) == null){  console.log('errrrrrrror'); return; }
-        return key + '=' + localStorage.getItem(key) + ';';
+        if(this._cookieService.get(key) == null){  console.log('errrrrrrror'); return; }
+        return key + '=' + this._cookieService.get(key) + ';';
     }
 
-    constructor(private http: Http, private _configuration: Configuration) {
+    constructor(private http: Http, private _configuration: Configuration, _cookieService:CookieService) {
+	this._cookieService = _cookieService;
         this.actionUrl = _configuration.ServerWithApiUrl;
-        this.headers = new Headers();
 
-        var cookiesValue = this.createKey(this.connect_sid) + this.createKey(this.access_token) + this.createKey(this.userId);
-        console.log("Cookies " , cookiesValue);
-        this.headers.append(this.cookies , cookiesValue);
-        this.headers.append('Content-Type', 'application/json');
-        this.headers.append('Accept', 'application/json');
     }
 
     public getAll(ns: string): Observable<Type[]> {
         console.log('GetAll ' + ns + ' to ' + this.actionUrl + ns);
-        return this.http.get(`${this.actionUrl}${ns}`)
+	this.headers = new Headers();
+	
+        var cookiesValue = this.createKey(this.connect_sid) + this.createKey(this.access_token) + this.createKey(this.userId);
+        console.log("Cookie " , cookiesValue);
+        this.headers.append('Cookie' , cookiesValue);
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Accept', 'application/json');
+        return this.http.get(`${this.actionUrl}${ns}` , {withCredentials: true,headers: this.headers})
           .map(this.extractData)
           .catch(this.handleError);
     }
